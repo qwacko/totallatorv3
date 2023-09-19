610ea036d44db51e0f5475c9adeb3af0c67c671c
qwacko <james@jmcdowall.com>
2023-09-19 10:17:08 +0000

    Fixed Pagination

--- src/lib/components/helpers/paginationHelper.ts
+++ src/lib/components/helpers/paginationHelper.ts
@@ -9,24 +9,39 @@ export const paginationHelper = ({
 	perPage: number;
 	buttonCount: number;
 }) => {
-	const maxPages = Math.ceil(count / perPage) - 1;
-	const startingPage = Math.max(0, page - buttonCount);
-	const endingPage = Math.min(page + buttonCount, maxPages);
-	const buttonsToShow = Math.round(endingPage - startingPage) + 1;
-	const enableNext = page < maxPages;
-	const enableLast = page < maxPages;
+	const maxPages = Math.ceil(count / perPage);
+	const halfRange = Math.floor(buttonCount / 2);
+
+	let startingPage = page - halfRange;
+	let endingPage = page + halfRange;
+
+	// Adjust if we're too close to the start
+	if (startingPage < 0) {
+		endingPage -= startingPage; // subtracting a negative number is the same as adding its absolute value
+		startingPage = 0;
+	}
+
+	// Adjust if we're too close to the end
+	if (endingPage >= maxPages) {
+		startingPage -= endingPage - maxPages + 1;
+		endingPage = maxPages - 1;
+	}
+
+	// Ensure startingPage is not negative
+	startingPage = Math.max(0, startingPage);
+
+	const enableNext = page < maxPages - 1;
+	const enableLast = page < maxPages - 1;
 	const enablePrev = page > 0;
 	const enableFirst = page > 0;
 
-	const returnArray = Array(buttonsToShow)
-		.fill('this')
-		.map((_, index) => {
-			return {
-				page: startingPage + index,
-				current: startingPage + index === page,
-				title: (startingPage + index + 1).toString()
-			};
-		});
+	const returnArray = Array.from({ length: endingPage - startingPage + 1 }, (_, index) => {
+		return {
+			page: startingPage + index,
+			current: startingPage + index === page,
+			title: (startingPage + index + 1).toString()
+		};
+	});
 
 	return {
 		enableFirst,
@@ -35,8 +50,8 @@ export const paginationHelper = ({
 		enableNext,
 		firstPage: 0,
 		prevPage: enableFirst ? page - 1 : 0,
-		lastPage: maxPages,
-		nextPage: enableLast ? page + 1 : maxPages,
+		lastPage: maxPages - 1,
+		nextPage: enableLast ? page + 1 : maxPages - 1,
 		maxPages,
 		buttons: returnArray
 	};
