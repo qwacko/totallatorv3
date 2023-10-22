b9b805fdf75f4ffcb8a65d6fbfb9efcb87e564e2
qwacko <james@jmcdowall.com>
2023-10-22 18:32:53 +0000

    Made create transaction work. still needs to redirect correctly.

--- src/routes/(loggedIn)/journals/create/+page.svelte
+++ src/routes/(loggedIn)/journals/create/+page.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
+	import CurrencyInputForm from '$lib/components/CurrencyInputForm.svelte';
 	import CustomHeader from '$lib/components/CustomHeader.svelte';
 	import ErrorText from '$lib/components/ErrorText.svelte';
 	import PageLayout from '$lib/components/PageLayout.svelte';
-	import TextInput from '$lib/components/TextInput.svelte';
+	import TextInputForm from '$lib/components/TextInputForm.svelte';
+	import DateInputForm from '$lib/components/DateInputForm.svelte';
 	import type { CreateSimpleTransactionSuperType } from '$lib/schema/journalSchema.js';
 	import { Button } from 'flowbite-svelte';
 	import { superForm } from 'sveltekit-superforms/client';
+	import CreateTransactionLinksForm from './CreateTransactionLinksForm.svelte';
+	import CreateTransactionLabelsForm from './CreateTransactionLabelsForm.svelte';
 
 	export let data;
 
-	const { form, errors, constraints, message, enhance } =
-		superForm<CreateSimpleTransactionSuperType>(data.form);
+	const form = superForm<CreateSimpleTransactionSuperType>(data.form);
+
+	$: enhance = form.enhance;
+	$: message = form.message;
 </script>
 
 <CustomHeader pageTitle="Create Transaction" />
 
 <PageLayout title="Create Transaction" size="lg">
 	<form method="POST" use:enhance class="flex flex-col gap-2">
-		<TextInput
-			title="Description"
-			errorMessage={$errors.description}
-			name="title"
-			bind:value={$form.description}
-			{...$constraints.description}
-		/>
+		<TextInputForm title="Description" {form} field="description" />
+		<DateInputForm title="Date" {form} field="date" />
+		<CurrencyInputForm title="Amount" {form} field="amount" />
+		<CreateTransactionLinksForm {form} dropdownInfo={data.dropdownInfo} />
+		<CreateTransactionLabelsForm {form} dropdownInfo={data.dropdownInfo} />
 		<Button type="submit">Create Transaction</Button>
 		<ErrorText message={$message} />
 	</form>
---
+++ src/routes/(loggedIn)/journals/create/CreateTransactionLinksForm.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import ComboSelectForm from '$lib/components/ComboSelectForm.svelte';
+	import type { CreateSimpleTransactionSuperType } from '$lib/schema/journalSchema';
+
+	import type { SuperForm } from 'sveltekit-superforms/client';
+
+	export let form: SuperForm<CreateSimpleTransactionSuperType>;
+
+	type DDI = { id: string; title: string; group: string; enabled: boolean };
+	type DDINoGroup = { id: string; title: string; enabled: boolean };
+	export let dropdownInfo: {
+		tags: Promise<DDI[]>;
+		bills: Promise<DDINoGroup[]>;
+		budgets: Promise<DDINoGroup[]>;
+		categories: Promise<DDI[]>;
+		accounts: Promise<DDI[]>;
+	};
+</script>
+
+<ComboSelectForm
+	{form}
+	title="From Account"
+	items={dropdownInfo.accounts}
+	field="fromAccountId"
+	placeholder="Select Account..."
+	itemToDisplay={(item) => ({ title: item.title, group: item.group })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
+<ComboSelectForm
+	{form}
+	title="to Account"
+	items={dropdownInfo.accounts}
+	field="toAccountId"
+	placeholder="Select Account..."
+	itemToDisplay={(item) => ({ title: item.title, group: item.group })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
+
+<ComboSelectForm
+	{form}
+	title="Tag"
+	items={dropdownInfo.tags}
+	field="tagId"
+	placeholder="Select Tag..."
+	itemToDisplay={(item) => ({ title: item.title, group: item.group })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
+<ComboSelectForm
+	{form}
+	title="Category"
+	items={dropdownInfo.categories}
+	field="categoryId"
+	placeholder="Select Category..."
+	itemToDisplay={(item) => ({ title: item.title, group: item.group })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
+<ComboSelectForm
+	{form}
+	title="Bill"
+	items={dropdownInfo.bills}
+	field="billId"
+	placeholder="Select Bill..."
+	itemToDisplay={(item) => ({ title: item.title })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
+<ComboSelectForm
+	{form}
+	title="Budget"
+	items={dropdownInfo.budgets}
+	field="budgetId"
+	placeholder="Select Budget..."
+	itemToDisplay={(item) => ({ title: item.title })}
+	itemToOption={(item) => ({ label: item.title, value: item.id, disabled: !item.enabled })}
+/>
