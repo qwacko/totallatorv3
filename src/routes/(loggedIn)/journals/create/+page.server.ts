264998c064593b432561928ba69d1884f4b02ec1
qwacko <james@jmcdowall.com>
2023-10-22 18:48:17 +0000

    Made Redirect Work Correctly.

--- src/routes/(loggedIn)/journals/create/+page.server.ts
+++ src/routes/(loggedIn)/journals/create/+page.server.ts
@@ -1,5 +1,9 @@
 import { authGuard } from '$lib/authGuard/authGuardConfig.js';
-import { createSimpleTransactionSchemaCore } from '$lib/schema/journalSchema.js';
+import {
+	createSimpleTransactionSchemaCore,
+	journalFilterSchema
+} from '$lib/schema/journalSchema.js';
+import { pageAndFilterValidation } from '$lib/schema/pageAndFilterValidation';
 import { tActions } from '$lib/server/db/actions/tActions.js';
 import { db } from '$lib/server/db/db.js';
 import { logging } from '$lib/server/logging';
@@ -14,12 +18,22 @@ export const load = async (data) => {
 	return { form };
 };
 
+const createValidation = createSimpleTransactionSchemaCore.merge(pageAndFilterValidation);
+
 export const actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request, createSimpleTransactionSchemaCore);
+		const form = await superValidate(request, createValidation);
 
 		if (!form.valid) {
+			logging.error('Update Form Is Not Valid', form.errors);
 			return { form };
+		}
+
+		const parsedFilter = journalFilterSchema.safeParse(JSON.parse(form.data.filter));
+
+		if (!parsedFilter.success) {
+			logging.error('Update Filter Is Not Valid', parsedFilter.error);
+			throw redirect(302, form.data.currentPage);
 		}
 
 		try {
@@ -28,6 +42,6 @@ export const actions = {
 			logging.info('Create Transaction Error', e);
 			return message(form, 'Error Creating Transaction');
 		}
-		throw redirect(302, '/journals');
+		throw redirect(302, form.data.prevPage);
 	}
 };
--- src/routes/(loggedIn)/journals/create/+page.svelte
+++ src/routes/(loggedIn)/journals/create/+page.svelte
@@ -10,11 +10,16 @@
 	import { superForm } from 'sveltekit-superforms/client';
 	import CreateTransactionLinksForm from './CreateTransactionLinksForm.svelte';
 	import CreateTransactionLabelsForm from './CreateTransactionLabelsForm.svelte';
+	import { page } from '$app/stores';
+	import { pageInfo } from '$lib/routes';
+	import RawDataModal from '$lib/components/RawDataModal.svelte';
+	import PreviousUrlInput from '$lib/components/PreviousURLInput.svelte';
 
 	export let data;
 
 	const form = superForm<CreateSimpleTransactionSuperType>(data.form);
 
+	$: urlInfo = pageInfo('/(loggedIn)/journals/create', $page);
 	$: enhance = form.enhance;
 	$: message = form.message;
 </script>
@@ -22,7 +27,16 @@
 <CustomHeader pageTitle="Create Transaction" />
 
 <PageLayout title="Create Transaction" size="lg">
+	<RawDataModal
+		dev={data.dev}
+		data={urlInfo.current.searchParams}
+		buttonText="Current Search Params"
+	/>
+	<div class="flex">{urlInfo.current.url}</div>
 	<form method="POST" use:enhance class="flex flex-col gap-2">
+		<PreviousUrlInput name="prevPage" />
+		<input type="hidden" name="filter" value={JSON.stringify(urlInfo.current.searchParams)} />
+		<input type="hidden" name="currentPage" value={urlInfo.current.url} />
 		<TextInputForm title="Description" {form} field="description" />
 		<DateInputForm title="Date" {form} field="date" />
 		<CurrencyInputForm title="Amount" {form} field="amount" />
