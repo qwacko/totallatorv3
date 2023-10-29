29393785284b715732946aeec88f00df81a80880
qwacko <james@jmcdowall.com>
2023-10-29 23:47:10 +0000

    Made query logging configurable.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fc5f5acf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.kn5rjcfj678"
+    "revision": "0.hma98fqcel8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
