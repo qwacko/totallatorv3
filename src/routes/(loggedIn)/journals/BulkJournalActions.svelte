12457af91873f631f7062e787cff015c853c6eae
qwacko <james@jmcdowall.com>
2023-11-20 23:29:55 +0000

    Improved Journal Actions

--- src/routes/(loggedIn)/journals/BulkJournalActions.svelte
+++ src/routes/(loggedIn)/journals/BulkJournalActions.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import ArrowRightIcon from '$lib/components/icons/ArrowRightIcon.svelte';
 	import CloneIcon from '$lib/components/icons/CloneIcon.svelte';
 	import DeleteIcon from '$lib/components/icons/DeleteIcon.svelte';
 	import EditIcon from '$lib/components/icons/EditIcon.svelte';
 	import EyeIcon from '$lib/components/icons/EyeIcon.svelte';
+	import FilterIcon from '$lib/components/icons/FilterIcon.svelte';
 	import { urlGenerator } from '$lib/routes';
 	import { defaultAllJournalFilter, type JournalFilterSchemaType } from '$lib/schema/journalSchema';
-	import { P, ButtonGroup, Button } from 'flowbite-svelte';
+	import { P, ButtonGroup, Button, Dropdown, DropdownItem } from 'flowbite-svelte';
 
 	export let selectedIds: string[];
 	export let searchParams: JournalFilterSchemaType | undefined;
@@ -18,6 +20,7 @@
 	</P>
 	<ButtonGroup>
 		<Button
+			class="p-2"
 			color="light"
 			href={urlGenerator({
 				address: '/(loggedIn)/journals/bulkEdit',
@@ -31,6 +34,7 @@
 			<EditIcon />
 		</Button>
 		<Button
+			class="p-2"
 			color="light"
 			href={urlGenerator({
 				address: '/(loggedIn)/journals/clone',
@@ -44,6 +48,7 @@
 			<CloneIcon />
 		</Button>
 		<Button
+			class="p-2"
 			color="light"
 			href={urlGenerator({
 				address: '/(loggedIn)/journals/delete',
@@ -57,6 +62,7 @@
 			<DeleteIcon />
 		</Button>
 		<Button
+			class="p-2"
 			color="light"
 			href={urlGenerator({
 				address: '/(loggedIn)/journals',
@@ -71,46 +77,230 @@
 			<EyeIcon />
 		</Button>
 	</ButtonGroup>
-
-	<P size="sm" weight="semibold" class="w-max whitespace-nowrap">
-		All ({allCount})
-	</P>
-	<ButtonGroup>
-		<Button
-			color="light"
-			href={urlGenerator({
-				address: '/(loggedIn)/journals/bulkEdit',
-				searchParamsValue: searchParams
-					? { ...searchParams, pageSize: 100000, page: 0 }
-					: searchParams
-			}).url}
-			disabled={allCount === 0}
-		>
-			<EditIcon />
-		</Button>
-		<Button
-			color="light"
-			href={urlGenerator({
-				address: '/(loggedIn)/journals/clone',
-				searchParamsValue: searchParams
-					? { ...searchParams, pageSize: 100000, page: 0 }
-					: searchParams
-			}).url}
-			disabled={allCount === 0}
-		>
-			<CloneIcon />
-		</Button>
-		<Button
-			color="light"
-			href={urlGenerator({
-				address: '/(loggedIn)/journals/delete',
-				searchParamsValue: searchParams
-					? { ...searchParams, pageSize: 100000, page: 0 }
-					: searchParams
-			}).url}
-			disabled={allCount === 0}
-		>
-			<DeleteIcon />
-		</Button>
-	</ButtonGroup>
+	<Button outline color="light" size="xs">All Journal Actions ({allCount})</Button>
+	<Dropdown>
+		<DropdownItem>
+			<div class="flex flex-row gap-1 items-center justify-between">All<ArrowRightIcon /></div>
+		</DropdownItem>
+		<Dropdown>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/bulkEdit',
+					searchParamsValue: searchParams
+						? { ...searchParams, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><EditIcon />Edit</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/clone',
+					searchParamsValue: searchParams
+						? { ...searchParams, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><CloneIcon />Clone</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/delete',
+					searchParamsValue: searchParams
+						? { ...searchParams, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><DeleteIcon />Delete</div>
+			</DropdownItem>
+		</Dropdown>
+		<DropdownItem>
+			<div class="flex flex-row gap-1 items-center justify-between">
+				Incomplete<ArrowRightIcon />
+			</div>
+		</DropdownItem>
+		<Dropdown>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/bulkEdit',
+					searchParamsValue: searchParams
+						? { ...searchParams, complete: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><EditIcon />Edit</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/clone',
+					searchParamsValue: searchParams
+						? { ...searchParams, complete: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><CloneIcon />Clone</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/delete',
+					searchParamsValue: searchParams
+						? { ...searchParams, complete: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><DeleteIcon />Delete</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals',
+					searchParamsValue: searchParams
+						? { ...searchParams, complete: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><FilterIcon />View</div>
+			</DropdownItem>
+		</Dropdown>
+		<DropdownItem>
+			<div class="flex flex-row gap-1 items-center justify-between">
+				Data Unchecked<ArrowRightIcon />
+			</div>
+		</DropdownItem>
+		<Dropdown>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/bulkEdit',
+					searchParamsValue: searchParams
+						? { ...searchParams, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><EditIcon />Edit</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/clone',
+					searchParamsValue: searchParams
+						? { ...searchParams, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><CloneIcon />Clone</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/delete',
+					searchParamsValue: searchParams
+						? { ...searchParams, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><DeleteIcon />Delete</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals',
+					searchParamsValue: searchParams
+						? { ...searchParams, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><FilterIcon />View</div>
+			</DropdownItem>
+		</Dropdown>
+		<DropdownItem>
+			<div class="flex flex-row gap-1 items-center justify-between">
+				Unreconciled<ArrowRightIcon />
+			</div>
+		</DropdownItem>
+		<Dropdown>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/bulkEdit',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><EditIcon />Edit</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/clone',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><CloneIcon />Clone</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/delete',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><DeleteIcon />Delete</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><FilterIcon />View</div>
+			</DropdownItem>
+		</Dropdown>
+		<DropdownItem>
+			<div class="flex flex-row gap-2 items-center justify-between">
+				Unchecked and Incomplete<ArrowRightIcon />
+			</div>
+		</DropdownItem>
+		<Dropdown>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/bulkEdit',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><EditIcon />Edit</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/clone',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><CloneIcon />Clone</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals/delete',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><DeleteIcon />Delete</div>
+			</DropdownItem>
+			<DropdownItem
+				href={urlGenerator({
+					address: '/(loggedIn)/journals',
+					searchParamsValue: searchParams
+						? { ...searchParams, reconciled: false, dataChecked: false, pageSize: 100000, page: 0 }
+						: undefined
+				}).url}
+			>
+				<div class="flex flex-row gap-1 items-center"><FilterIcon />View</div>
+			</DropdownItem>
+		</Dropdown>
+	</Dropdown>
 </div>
