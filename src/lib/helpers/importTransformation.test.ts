0ed3b652f1a92989dde06906f7feff1b9269e3f6
qwacko <james@jmcdowall.com>
2023-11-21 17:32:33 +0000

    Switch import mapping to use handlebars

--- src/lib/helpers/importTransformation.test.ts
+++ src/lib/helpers/importTransformation.test.ts
@@ -4,39 +4,29 @@ import { processConfigString } from './importTransformation'; // Adjust the impo
 describe('processConfigString', () => {
 	it('should correctly replace substring operations', () => {
 		const inputObject = { description: 'This is a description' };
-		const configString = '{description:substring:2:8}';
+		const configString = '{{substring description 2 8}}';
 		const result = processConfigString(configString, inputObject);
 		expect(result).toBe('is is a ');
 	});
 
 	it('should correctly replace regex operations', () => {
 		const inputObject = { text: 'Hello World' };
-		const configString = '{text:regex:o:0}';
+		const configString = '{{regexReplace text "o" "0"}}';
 		const result = processConfigString(configString, inputObject);
 		expect(result).toBe('Hell0 W0rld');
 	});
 
 	it('should correctly replace multiply operations', () => {
 		const inputObject = { number: '10' };
-		const configString = '{number:multiply:2}';
+		const configString = '{{multiply number 2}}';
 		const result = processConfigString(configString, inputObject);
 		expect(result).toBe('20');
 	});
 
 	it('should throw an error for missing keys', () => {
 		const inputObject = { description: 'This is a description' };
-		const configString = '{nonExistentKey:substring:2:5}';
-		expect(() => processConfigString(configString, inputObject)).toThrow(
-			/Key 'nonExistentKey' not found/
-		);
-	});
-
-	it('should throw an error for unsupported operations', () => {
-		const inputObject = { description: 'This is a description' };
-		const configString = '{description:unsupportedOperation:2:5}';
-		expect(() => processConfigString(configString, inputObject)).toThrow(
-			/Unsupported operation 'unsupportedOperation'/
-		);
+		const configString = '{{substring nonExistentKey 2 5}}';
+		expect(() => processConfigString(configString, inputObject)).toThrow('Cannot read properties');
 	});
 
 	it('should handle complex configurations', () => {
@@ -45,8 +35,25 @@ describe('processConfigString', () => {
 			lastName: 'Doe',
 			age: '30'
 		};
-		const configString = '{firstName:direct} {lastName:direct} is {age:multiply:2} years old';
+		const configString = '{{firstName}} {{lastName}} is {{multiply age 2}} years old';
 		const result = processConfigString(configString, inputObject);
 		expect(result).toBe('John Doe is 60 years old');
+	});
+
+	it('should correctly handle conditional operations', () => {
+		const inputObject = {
+			status: 'active',
+			firstName: 'Jane',
+			lastName: 'Doe'
+		};
+		const configString = "{{#if (eq status 'active')}}{{firstName}}{{else}}{{lastName}}{{/if}}";
+
+		const result = processConfigString(configString, inputObject);
+		expect(result).toBe('Jane');
+
+		const configStringInactive =
+			"{{#if (eq status 'disabled')}}{{firstName}}{{else}}{{lastName}}{{/if}}";
+		const resultInactive = processConfigString(configStringInactive, inputObject);
+		expect(resultInactive).toBe('Doe');
 	});
 });
