3f35c59a145ae9c0dd3e901193a03c5d3bbbd526
qwacko <james@jmcdowall.com>
2023-11-26 05:46:46 +0000

    Fixed Buffering Data Functionality.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fc5f5acf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.8ao6jesqlkg"
+    "revision": "0.j12i2d0r8e8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
