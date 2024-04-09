3215a6bf3fa00c57b377e029e370254d66bf1980
qwacko <43203327+qwacko@users.noreply.github.com>
2024-04-09 13:32:05 +1200

    Fix Label and payee subqueries (#108)
    * Limited lookup lenght and removed unused data.
    
    * Minor fix to schema mapping.
    
    ---------
    
    Co-authored-by: james <james@jmcdowall.com>

--- src/lib/server/db/actions/helpers/journal/journalList.ts
+++ src/lib/server/db/actions/helpers/journal/journalList.ts
@@ -3,23 +3,16 @@ import {
 	defaultJournalFilter,
 	type JournalFilterSchemaInputType
 } from '$lib/schema/journalSchema';
-import { getTableColumns, eq, and, sum, sql, not, or } from 'drizzle-orm';
+import { eq, and, sum, sql, not, or } from 'drizzle-orm';
 import type { DBType } from '../../../db';
 import {
 	account,
 	journalEntry,
-	bill,
-	budget,
-	category,
-	tag,
 	label,
 	labelsToJournals,
-	importTable,
 	importItemDetail
 } from '../../../postgres/schema';
 import { journalExtendedView } from '../../../postgres/schema/materializedViewSchema';
-import { journalFilterToQuery } from './journalFilterToQuery';
-import { journalFilterToOrderBy } from './journalFilterToOrderBy';
 import { count as drizzleCount } from 'drizzle-orm';
 import { materializedJournalFilterToQuery } from '../journalMaterializedView/materializedJournalFilterToQuery';
 import { materializedJournalFilterToOrderBy } from '../journalMaterializedView/materializedJournalFilterToOrderBy';
@@ -104,11 +97,14 @@ export const journalMaterialisedList = async ({
 
 	const { page = 0, pageSize = 10, ...restFilter } = processedFilter;
 
+	const andFilter = await materializedJournalFilterToQuery(db, restFilter);
+	const orderBy = materializedJournalFilterToOrderBy(processedFilter);
+
 	const journalQueryCore = db
 		.select()
 		.from(journalExtendedView)
-		.where(and(...(await materializedJournalFilterToQuery(db, restFilter))))
-		.orderBy(...materializedJournalFilterToOrderBy(processedFilter))
+		.where(and(...andFilter))
+		.orderBy(...orderBy)
 		.offset(page * pageSize)
 		.limit(pageSize);
 
@@ -119,8 +115,8 @@ export const journalMaterialisedList = async ({
 	const runningTotalInner = db
 		.select({ amount: journalExtendedView.amount })
 		.from(journalExtendedView)
-		.where(and(...(await materializedJournalFilterToQuery(db, restFilter))))
-		.orderBy(...materializedJournalFilterToOrderBy(processedFilter))
+		.where(and(...andFilter))
+		.orderBy(...orderBy)
 		.offset(page * pageSize)
 		.as('sumInner');
 
@@ -134,7 +130,7 @@ export const journalMaterialisedList = async ({
 			count: drizzleCount(journalExtendedView.id)
 		})
 		.from(journalExtendedView)
-		.where(and(...(await materializedJournalFilterToQuery(db, restFilter))))
+		.where(and(...andFilter))
 		.execute();
 
 	const count = resultCount[0].count;
@@ -145,14 +141,17 @@ export const journalMaterialisedList = async ({
 
 	const runningTotal = (await runningTotalPromise)[0].sum;
 
-	const otherJournalData = await getOtherJournalInfo(db, journalIds);
+	//Logic to prevent querying too much data if the page size is too large
+	const limitedJournalIds = journalIds.slice(0, 500);
+
+	const otherJournalData = await getOtherJournalInfo(db, limitedJournalIds);
 	const importDetails = await db
 		.select()
 		.from(importItemDetail)
 		.where(
 			or(
-				inArrayWrapped(importItemDetail.relationId, journalIds),
-				inArrayWrapped(importItemDetail.relation2Id, journalIds)
+				inArrayWrapped(importItemDetail.relationId, limitedJournalIds),
+				inArrayWrapped(importItemDetail.relation2Id, limitedJournalIds)
 			)
 		)
 		.execute();
@@ -172,144 +171,6 @@ export const journalMaterialisedList = async ({
 			otherJournals: thisOtherJournalData?.otherJournals ?? [],
 			importDetail: thisImportDetail?.processedInfo,
 			labels: thisOtherJournalData?.labels ?? []
-		};
-	});
-
-	return {
-		count,
-		data: journalsMerged,
-		pageCount,
-		page,
-		pageSize,
-		runningTotal
-	};
-};
-
-export const journalList = async ({
-	db,
-	filter
-}: {
-	db: DBType;
-	filter: JournalFilterSchemaInputType;
-}) => {
-	const processedFilter = journalFilterSchema.catch(defaultJournalFilter()).parse(filter);
-
-	const { page = 0, pageSize = 10, ...restFilter } = processedFilter;
-
-	const journalQueryCore = db
-		.select({
-			...getTableColumns(journalEntry),
-			tagTitle: tag.title,
-			billTitle: bill.title,
-			budgetTitle: budget.title,
-			categoryTitle: category.title,
-			accountTitle: account.title,
-			accountType: account.type,
-			accountGroup: account.accountGroupCombined,
-			importTitle: importTable.title
-		})
-		.from(journalEntry)
-		.leftJoin(account, eq(journalEntry.accountId, account.id))
-		.leftJoin(bill, eq(journalEntry.billId, bill.id))
-		.leftJoin(budget, eq(journalEntry.budgetId, budget.id))
-		.leftJoin(category, eq(journalEntry.categoryId, category.id))
-		.leftJoin(tag, eq(journalEntry.tagId, tag.id))
-		.leftJoin(importTable, eq(journalEntry.importId, importTable.id))
-		.where(and(...(await journalFilterToQuery(db, restFilter))))
-		.orderBy(...journalFilterToOrderBy(processedFilter))
-		.offset(page * pageSize)
-		.limit(pageSize);
-
-	const journalsPromise = journalQueryCore.execute();
-
-	const runningTotalInner = db
-		.select({
-			amount: getTableColumns(journalEntry).amount
-		})
-		.from(journalEntry)
-		.leftJoin(account, eq(journalEntry.accountId, account.id))
-		.leftJoin(bill, eq(journalEntry.billId, bill.id))
-		.leftJoin(budget, eq(journalEntry.budgetId, budget.id))
-		.leftJoin(category, eq(journalEntry.categoryId, category.id))
-		.leftJoin(tag, eq(journalEntry.tagId, tag.id))
-		.leftJoin(importTable, eq(journalEntry.importId, importTable.id))
-		.where(and(...(await journalFilterToQuery(db, restFilter))))
-		.orderBy(...journalFilterToOrderBy(processedFilter))
-		.offset(page * pageSize)
-		// .limit(-1)
-		.as('sumInner');
-
-	const runningTotalPromise = db
-		.select({ sum: sum(runningTotalInner.amount).mapWith(Number) })
-		.from(runningTotalInner)
-		.execute();
-
-	const resultCount = await db
-		.select({
-			count: drizzleCount(journalEntry.id)
-		})
-		.from(journalEntry)
-		.leftJoin(account, eq(journalEntry.accountId, account.id))
-		.leftJoin(bill, eq(journalEntry.billId, bill.id))
-		.leftJoin(budget, eq(journalEntry.budgetId, budget.id))
-		.leftJoin(category, eq(journalEntry.categoryId, category.id))
-		.leftJoin(tag, eq(journalEntry.tagId, tag.id))
-		.where(and(...(await journalFilterToQuery(db, restFilter))))
-		.execute();
-
-	const count = resultCount[0].count;
-	const pageCount = Math.max(1, Math.ceil(count / pageSize));
-
-	const journals = await journalsPromise;
-
-	const journalIds = journals.map((item) => item.id);
-
-	const journalLabels = await db
-		.select({ journalId: journalEntry.id, labelId: label.id, labelTitle: label.title })
-		.from(journalEntry)
-		.leftJoin(labelsToJournals, eq(labelsToJournals.journalId, journalEntry.id))
-		.leftJoin(label, eq(label.id, labelsToJournals.labelId))
-		.where(inArrayWrapped(journalEntry.id, journalIds))
-		.execute();
-
-	const runningTotal = (await runningTotalPromise)[0].sum;
-
-	const transactionIds = journals.map((journal) => journal.transactionId);
-
-	const transactionJournals =
-		transactionIds.length > 0
-			? await db
-					.select({
-						id: journalEntry.id,
-						transactionId: journalEntry.transactionId,
-						accountId: journalEntry.accountId,
-						accountTitle: account.title,
-						accountType: account.type,
-						accountGroup: account.accountGroupCombined,
-						amount: journalEntry.amount
-					})
-					.from(journalEntry)
-					.leftJoin(account, eq(journalEntry.accountId, account.id))
-					.where(inArrayWrapped(journalEntry.transactionId, transactionIds))
-					.execute()
-			: [];
-
-	const journalsMerged = journals.map((journal, index) => {
-		const otherJournals = transactionJournals.filter(
-			(x) => x.transactionId === journal.transactionId && x.id !== journal.id
-		);
-		const priorJournals = journals.filter((_, i) => i < index);
-		const priorJournalTotal = priorJournals.reduce((prev, current) => prev + current.amount, 0);
-		const total = runningTotal - priorJournalTotal;
-		const labels = journalLabels
-			.filter((item) => item.journalId === journal.id)
-			.map((item) => ({ id: item.labelId, title: item.labelTitle }));
-
-		return {
-			...journal,
-			total,
-			otherJournals,
-			labels
 		};
 	});
 
