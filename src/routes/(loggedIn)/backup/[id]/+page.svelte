3a768c872662ca8c040b55d7511d400872f606dd
james <james@jmcdowall.com>
2024-04-18 21:23:59 +0000

    Added files and notes to backup.

--- src/routes/(loggedIn)/backup/[id]/+page.svelte
+++ src/routes/(loggedIn)/backup/[id]/+page.svelte
@@ -129,6 +129,8 @@
 					<Badge>Report Items : {information.information.itemCount.numberReportItems}</Badge>
 					<Badge>Reports : {information.information.itemCount.numberReports}</Badge>
 					<Badge>Backups : {information.information.itemCount.numberBackups}</Badge>
+					<Badge>Files : {information.information.itemCount.numberFiles}</Badge>
+					<Badge>Notes : {information.information.itemCount.numberNotes}</Badge>
 				</div>
 			</div>
 		</div>
