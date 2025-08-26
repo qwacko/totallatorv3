40551fe57a22287efeb888c48162f389aca7ad8c
james <james@jmcdowall.com>
2025-08-26 23:09:00 +0000

    Added migration to fix collation version issue

---
+++ packages/database/src/migrations/0071_safe_tarot.sql
@@ -0,0 +1,6 @@
+-- Custom SQL migration file, put your code below! --
+
+-- Refresh collation version to resolve PostgreSQL collation version mismatch warnings
+-- This fixes the warning: "database has a collation version mismatch"
+-- which commonly occurs when running in Docker containers with different glibc versions
+ALTER DATABASE totallatorv3 REFRESH COLLATION VERSION;
