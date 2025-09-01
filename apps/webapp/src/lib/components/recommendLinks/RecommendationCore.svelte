39b2b70d06e630a0ba4ef578a92e0dc944b00203
james <james@jmcdowall.com>
2025-09-01 10:31:48 +0000

    Improved Recommendation Display

--- apps/webapp/src/lib/components/recommendLinks/RecommendationCore.svelte
+++ apps/webapp/src/lib/components/recommendLinks/RecommendationCore.svelte
@@ -2,6 +2,8 @@
 	import { Button } from 'flowbite-svelte';
 	import { untrack } from 'svelte';
 
+	import LoadingSpinner from '../LoadingSpinner.svelte';
+
 	type ReturnType = {
 		recommendations: {
 			id: string;
@@ -41,17 +43,29 @@
 
 {#if currentId === undefined && delayedParams !== undefined}
 	<svelte:boundary>
-		<div class="flex flex-row flex-wrap gap-2 self-stretch">
-			{#each (await getItems(delayedParams)).recommendations as data}
-				<Button
-					class="@md:grow-0 @md:whitespace-nowrap flex grow basis-0"
-					color="alternative"
-					onclick={() => updateId(data.id)}
-				>
-					{(data.fraction * 100).toFixed(0)}% - {data.title}
-				</Button>
-			{/each}
-		</div>
-		{#snippet pending()}Loading recommendations...{/snippet}
+		{#await getItems(delayedParams) then result}
+			{@const maxFraction = Math.max(...result.recommendations.map((r) => r.fraction))}
+			<div class="@md:grid-cols-2 grid grid-cols-1 gap-2 self-stretch">
+				{#each result.recommendations as data}
+					<Button
+						class="relative justify-start overflow-hidden text-left"
+						color="alternative"
+						onclick={() => updateId(data.id)}
+					>
+						<div
+							class="absolute inset-0 bg-blue-100 opacity-30 transition-all duration-200 dark:bg-blue-900"
+							style="width: {(data.fraction / maxFraction) * 100}%"
+						></div>
+						<div class="relative z-10 flex w-full items-center justify-between">
+							<span class="truncate">{data.title}</span>
+							<span class="ml-2 shrink-0 text-xs font-medium">
+								{(data.fraction * 100).toFixed(0)}%
+							</span>
+						</div>
+					</Button>
+				{/each}
+			</div>
+		{/await}
+		{#snippet pending()}<LoadingSpinner loadingText="Loading Recommendations" />{/snippet}
 	</svelte:boundary>
 {/if}
